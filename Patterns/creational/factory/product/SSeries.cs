02526a0dc2a2994a91ac08a7acf137b1903da268
Majed Samyal <Majed_Samyal@epam.com>
2021-03-17 10:45:25 +0530

    intial setup with factory pattern

---
+++ Patterns/creational/factory/product/SSeries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.creational.factory
+{
+
+    /// <summary>
+    /// SSeries implementation of IMobile - Product
+    /// </summary>
+    public class SSeries : IMobile
+    {
+        public string ModelName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Prize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Patterns.creational.factory;
+
+namespace designpatters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            #region Creational
+            // Factory Design Pattern
+
+            SamsungFactory samsungFactory = null;
+            int budget = 15000;
+            switch (budget)
+            {
+                case 15000:
+                    samsungFactory = new PSeriesFactory();
+                    break;
+                case 10000:
+                    samsungFactory = new SSeriesFactory();
+                    break;
+                default:
+                    throw new NotImplementedException("");
+            }
+
+            var mobile = samsungFactory.CreateMobile();
+            Console.WriteLine($"Samsung mobile {mobile.ModelName} costs you {mobile.Prize}");
+
+
+            #endregion
+        }
+    }
+}
